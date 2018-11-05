086392cb1827c48c0ff7a67705499963da2b87a5
Henry Richardson <hrichardson83@gmail.com>
2018-11-05 16:22:03 -0500

    added index.js basic component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('test');
+function component() {
+    let element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());  

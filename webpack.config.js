584b31de3ce15bdc62febffec3aba2a16f888166
Henry Richardson <hrichardson83@gmail.com>
2018-11-05 18:34:03 -0500

    Cleaning up the /dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Output Management'
-      })
-    ],
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+    title: 'Output Management'
+    })
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')

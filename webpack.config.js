b0c116f4bdbb8c9384e3531458e49c5e8f4db429
Henry Richardson <hrichardson83@gmail.com>
2018-11-05 18:35:35 -0500

    Added Manifest to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
+    new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
     title: 'Output Management'

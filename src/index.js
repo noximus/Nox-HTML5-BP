e8cb0c35040074c58bd95ff2f0dbfcfefb599500
Henry Richardson <hrichardson83@gmail.com>
2018-11-05 19:23:44 -0500

    cleaned up files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './image1.png';
 
 function component() {
     let element = document.createElement('div');
   
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
   
-    var myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -17,27 +17,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-              'file-loader'
-          ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-            'file-loader'
-        ]
-      }
-    ]
-  }
 };

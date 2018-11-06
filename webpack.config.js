886b3c160863ac38825d97c57152428714b6a93a
Henry Richardson <hrichardson83@gmail.com>
2018-11-05 19:44:30 -0500

    added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
     print: './src/print.js'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),

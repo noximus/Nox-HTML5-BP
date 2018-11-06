8867426bce4dac08e2dc53c52b6d435636d024bf
Henry Richardson <hrichardson83@gmail.com>
2018-11-05 20:04:58 -0500

    Enabling HMR successful

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
@@ -14,13 +15,16 @@ function component() {
   return element;
 }
 
-document.body.appendChild(component());  
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element);
 
 // Hot Module Replacement: Enabling HMR
 if (module.hot) {
   module.hot.accept('./print.js', function() {
     console.log('Accepting the updated printMe module!');
-    printMe();
+    document.body.removeChild(element);
+    element = component(); // Re-render the "component" to update the click handler
+    document.body.appendChild(element);
   })
 }
     

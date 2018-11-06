20ee739dd7b105dfb132c0e68918ac681c5db062
Henry Richardson <hrichardson83@gmail.com>
2018-11-05 19:59:15 -0500

    Hot Module Replacement and Enabling HMR

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import _ from 'lodash';
 
 function component() {
-    let element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());  
+  var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+document.body.appendChild(component());  
+
+// Hot Module Replacement: Enabling HMR
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  })
+}
+    
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const webpack = require('webpack');
 
 
 module.exports = {
   mode: 'development',
-  entry: {
-    app: './src/index.js',
-    print: './src/print.js'
-  },
+  entry: { app: './src/index.js' },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   plugins: [
     new ManifestPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
+      title: 'HTML5 BoilerPlase'
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name]bundle.js',
